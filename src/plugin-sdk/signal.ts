743fd9e073091d4645dd044d4f3fdf3f1f642378
root <root@openclaw.quasireal.ca>
2026-03-29 10:02:50 +0000

    fix(signal): remove orphaned closing brace in plugin-sdk exports

--- src/plugin-sdk/signal.ts
+++ src/plugin-sdk/signal.ts
@@ -71,7 +71,6 @@ export {
   createDefaultChannelRuntimeState,
 } from "./status-helpers.js";
 
-} from "./signal-surface.js";
 export { isSignalSenderAllowed } from "./signal-surface.js";
 export type { SignalSender } from "./signal-surface.js";
 export { monitorSignalProvider } from "./signal-surface.js";
